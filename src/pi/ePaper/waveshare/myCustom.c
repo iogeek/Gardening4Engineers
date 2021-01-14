d0081a6222e72f5e2c7f85d28254f449515d049f
iogeek <iogeek@users.noreply.github.com>
2021-01-14 04:22:36 +0000

    Deploying to gh-pages from  @ 6f9a38c3999a2d04fe3d56f8ec2eb2e0c499f559 🚀

--- src/pi/ePaper/waveshare/myCustom.c
+++ src/pi/ePaper/waveshare/myCustom.c
@@ -103,3 +103,66 @@ bool printString(int x, int y, char *str) {
     printf("printString:end\n");
     return true;
 }
+
+
+
+void PrintString(UBYTE *pImage, int x, int y, int x2, int y2, bool resetRect, int fontSize, char *str, int width, int height, int rotation, int color) {
+    sFONT aFont;
+    printf("printString:start\n");
+
+    switch (fontSize) {
+        case 48:
+            aFont = Font48;
+            break;
+        case 36:
+            aFont = Font36;
+            break;
+        case 24:
+            aFont = Font24;
+            break;
+        case 20:
+            aFont = Font20;
+            break;
+        case 16:
+            aFont = Font16;
+            break;
+        case 12:
+            aFont = Font12;
+            break;
+        case 8:
+            aFont = Font8;
+            break;        
+        default:
+            aFont = Font8;
+            break;        
+    }
+    Paint_NewImage(pImage, width, height, rotation, color);
+    //sprintf(str, "%d", num);
+    if (resetRect) Paint_DrawRectangle(x, y, x2, y2, color, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Paint_DrawString_EN(x, y, str, &aFont, WHITE, BLACK);
+
+    EPD_2IN7_Display(pImage);
+
+}
+
+void DisplayBackground(UBYTE *pImage, char *bmpName, int width, int height, int rotation, int color)
+{       
+  printf("printString:start\n");
+  if(DEV_Module_Init()!=0){
+      printf("printString:end:init failed\n");
+      return;
+  }
+
+  printf("e-Paper Init and Clear...\r\n");
+  EPD_2IN7_Init();
+  EPD_2IN7_Clear();
+  DEV_Delay_ms(500);
+
+
+  printf("Paint_NewImage\r\n");
+  Paint_NewImage(pImage, width, height, rotation, color);
+  //Paint_Clear(WHITE);
+  GUI_ReadBmp(bmpName, 0, 0);
+  EPD_2IN7_Display(pImage);
+  printf("printString:end\n");
+}
