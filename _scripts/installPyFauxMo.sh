8c63e4d3b41d29507f65459209833105774c13fe
Robbie Harris <iogeek@outlook.com>
2021-07-18 20:06:53 -0700

    eos

--- _scripts/installPyFauxMo.sh
+++ _scripts/installPyFauxMo.sh
@@ -5,12 +5,17 @@ python3 pip install --upgrade pip
 python3 -m pip install fauxmo
 
 
-Make a config.json based on config-sample.json
-fauxmo -c config.json [-v]
-
-
 mkdir -p $HOME/fauxmo
 cd $HOME/fauxmo
 
-wget https://raw.githubusercontent.com/n8henrie/fauxmo/master/extras/fauxmo.service
+#wget https://raw.githubusercontent.com/n8henrie/fauxmo/master/extras/fauxmo.service
 wget https://raw.githubusercontent.com/n8henrie/fauxmo-plugins/master/mqttplugin.py
+
+fauxmo -c config.json
+
+sudo useradd -r -s /bin/false fauxmo
+
+sudo cp $HOME/fauxmo/fauxmo.service /etc/systemd/system/fauxmo.service
+
+sudo systemctl enable fauxmo.service
+sudo systemctl start fauxmo.service
