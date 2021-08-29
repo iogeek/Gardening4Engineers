6eef8b172c2bfb07a2ce0272373fdaad83dd583c
Robbie Harris <iogeek@outlook.com>
2021-08-29 16:20:39 -0700

    eos

---
+++ _scripts/setupLinux.sh
@@ -0,0 +1,10 @@
+# install dotnet.  Based on https://github.com/dotnet/install-scripts
+wget https://dot.net/v1/dotnet-install.sh
+#./dotnet-install.sh  -Channel Current -DryRun
+./dotnet-install.sh  -Channel Current
+rm dotnet-install.sh 
+
+
+ln -s $HOME/.dotnet/dotnet /usr/local/bin 
+echo ‘export DOTNET_ROOT=$HOME/.dotnet’ >> $HOME/.bashrc dotnet –info
+
