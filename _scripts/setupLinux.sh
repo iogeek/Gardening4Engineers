615993f13843caac06771f246d4f9e82b932c6c8
Robbie Harris <iogeek@outlook.com>
2021-08-29 20:33:13 -0700

    eos

--- _scripts/setupLinux.sh
+++ _scripts/setupLinux.sh
@@ -1,3 +1,6 @@
+ sudo apt -y update
+ sudo apt -y upgrade
+
 # install dotnet.  Based on https://github.com/dotnet/install-scripts
 wget https://dot.net/v1/dotnet-install.sh
 #./dotnet-install.sh  -Channel Current -DryRun
@@ -8,4 +11,10 @@ rm dotnet-install.sh
 ln -s $HOME/.dotnet/dotnet /usr/local/bin 
 echo ‘export DOTNET_ROOT=$HOME/.dotnet’ >> $HOME/.bashrc dotnet –info
 #
+
+ sudo apt-get install -y build-essential dos2unix git python3 ruby-full
+ 
+ # jekyll
+ gem update
+ gem install jekyll bundler minima
 
